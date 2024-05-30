c156744784cd37d40b724fa5e743ec2b32824771
lathifahdhy <148345529+lathifahdhy@users.noreply.github.com>
2024-05-30 18:01:50 +0700

    Create delete-ruang.php

---
+++ Ruang/delete-ruang.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+
+    $sql = "DELETE FROM rooms WHERE id=$id";
+
+    if (mysqli_query($db, $sql)) {
+        echo "Ruang berhasil dihapus.";
+    } else {
+        echo "Kesalahan: " . $sql . "<br>" . mysqli_error($db);
+    }
+}
+?>
+
+<form method="POST">
+    ID: <input type="text" name="id" required><br>
+    <input type="submit" value="Hapus Ruang">
+</form>
